8ebfddf401b0c78f115b2393d9c6a44160f2fd2d
Olga <olga@leanconvert.com>
2020-10-08 16:09:44 +0300

    done arrays and objects

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    var result = fn.call(obj);
+    return result 
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
 
+    var arr = [];
+
+    for (var key in obj) {
+      if (obj.hasOwnProperty(key) === false) continue;
+      arr.push( key + ": " + obj[key] ); 
+    }
+    return arr
   }
 };
